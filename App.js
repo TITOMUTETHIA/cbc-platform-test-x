cd98ef94f896094cbc2444019da3e50d7c519557
TITO MUTETHIA <titomutethia@gmail.com>
2025-02-27 00:17:03 +0300

    new push

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { theme } from './src/config/theme';
+import AppNavigator from './src/navigation';
+import AuthProvider from './src/context/auth/AuthContext';
+
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <PaperProvider theme={theme}>
+        <AuthProvider>
+          <AppNavigator />
+        </AuthProvider>
+      </PaperProvider>
+    </SafeAreaProvider>
+  );
+};
+
+export default App;
